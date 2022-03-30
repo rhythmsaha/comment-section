95cb8c4217a1d09ab5af83aa77f75ebf07481169
Rhythm Saha <rs.2001.saha@gmail.com>
2022-03-30 09:24:06 +0530

    button hover

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ function Button({ onClick, children, type }) {
         <button
             type={type}
             onClick={onClick}
-            className="bg-indigo-500 text-white px-5 py-2 text-xs font-semibold rounded-md"
+            className="bg-indigo-500 hover:bg-indigo-400 text-white px-5 py-2 text-xs font-semibold rounded-md transition "
         >
             {children}
         </button>
